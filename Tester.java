2141981d2b7e104f510ef6693623412fda778d04
tmm65 <55416739+tmm65@users.noreply.github.com>
2019-10-07 23:46:28 -0500

    Uploaded initial test code
    Successfully accessed TestDB, searched for a specific invite code, extracted the associated H_id, and set "was_used" to "true".

---
+++ Tester.java
@@ -0,0 +1,15 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+	DatabaseConnection db = new DatabaseConnection();
+
+	db.acceptInvite("aRandomString");
+		
+		
+	} // end of main()
+	
+	
+
+} // end of Test class
