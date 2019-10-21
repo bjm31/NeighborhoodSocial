012a1b876ba53bab38b620d43320b8b70ebd161c
tmm65 <tmm65@students.uwf.edu>
2019-10-21 03:23:46 -0500

    working on basic login functionality

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,41 +1,41 @@
 package servlet;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import backend.DatabaseActions;
+import backend.*;
+import backend.exception.*;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/home")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String user = request.getParameter("username");
+		char[] pass = request.getParameter("password").toCharArray();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		
+		try {
+			if (DatabaseActions.login(user, pass)) {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/successfulLogin.html");
+//				dispatcher.forward(request, response);
+			}
+			else {
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/loginError.html");
+//				rd.include(request, response);
+			}
+		} catch (InvalidLoginException e) {}
+		
+	}	
+	
 
 }
