a5667364a9da88c9510dbbda970770a6f6befe18
tmm65 <tmm65@students.uwf.edu>
2019-10-20 21:07:34 -0500

    refactor

--- src/backend/exception/DatabaseConnectionFailureException.java
+++ src/backend/exception/DatabaseConnectionFailureException.java
@@ -9,4 +9,8 @@ package backend.exception;
  */
 public class DatabaseConnectionFailureException extends Exception{
 	static final long serialVersionUID = 2L;
+	
+	public DatabaseConnectionFailureException (String message) {
+		super(message);
+	}
 }
