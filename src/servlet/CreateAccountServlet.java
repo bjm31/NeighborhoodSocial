e34feaf5dfcc8dd9cb8f30a9e9c7226ab383a2ca
tmm65 <55416739+tmm65@users.noreply.github.com>
2019-11-02 21:35:16 -0500

    wrote and tested new account backend + servlet. see discord for restults.

---
+++ src/servlet/CreateAccountServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import backend.DatabaseActions;
+
+/**
+ * Servlet implementation class CreateAccountServlet
+ */
+@WebServlet("/CreateAccount")
+public class CreateAccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateAccountServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username 	= request.getParameter("username");
+		String fname		= request.getParameter("fname");
+		String lname		= request.getParameter("lname");
+		String email		= request.getParameter("email");
+		String photo		= request.getParameter("photo");
+		DatabaseActions.createNewUser("aRandomString", username, fname, lname, email, photo); //TODO fix invite code String		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
