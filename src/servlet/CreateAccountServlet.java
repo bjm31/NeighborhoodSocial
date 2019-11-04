574cb1991d7b6a607ce853d81226c17b75869f02
tmm65 <55416739+tmm65@users.noreply.github.com>
2019-11-03 23:15:04 -0600

    password creation implemented. can now create new user account and successfully authenticate with that account

--- src/servlet/CreateAccountServlet.java
+++ src/servlet/CreateAccountServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import backend.DatabaseActions;
+import java.util.Arrays;
 
 /**
  * Servlet implementation class CreateAccountServlet
@@ -32,7 +35,16 @@ public class CreateAccountServlet extends HttpServlet {
 		String lname		= request.getParameter("lname");
 		String email		= request.getParameter("email");
 		String photo		= request.getParameter("photo");
-		DatabaseActions.createNewUser("aRandomString", username, fname, lname, email, photo); //TODO fix invite code String		
+		char[] password1	= request.getParameter("password").toCharArray();
+		char[] password2	= request.getParameter("password_confirm").toCharArray();
+		
+		if (!Arrays.equals(password1, password2)) {
+			RequestDispatcher rd = request.getRequestDispatcher("badPasword.html");			//TODO  update with actual HTML page
+			rd.forward(request, response);
+		}
+		
+		DatabaseActions.createNewUser("aRandomString", username, fname, lname, email, photo); //TODO fix invite code String
+		DatabaseActions.setNewPassword(username, password1);
 	}
 
 	/**
